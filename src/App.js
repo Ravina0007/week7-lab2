88d919bcb763210b86b2e8d4fa9fa7918c692497
Ravina <raniravina71996@gmail.com>
2023-11-24 19:10:59 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+function Hello() {
+  return <p>Hello, Hello Ravina !</p>;
+}
 
+// App.js
+function Bye() {
+  return <p>Goodbye, Bye Ravina !</p>;
+}
+
+// App.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Bye />
     </div>
   );
 }
